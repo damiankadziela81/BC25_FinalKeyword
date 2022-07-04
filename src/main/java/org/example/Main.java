11175e8a9cb64686ff96f23e8a0070869408c17b
Damian Kadziela <damiankadziela02@gmail.com>
2022-07-04 17:56:50 +0200

    added some additional comments

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
         final double PI = 3.14159;
+        //once variable is declared final, it can't be changed later in the program
+        //common practice is to name the final variable in capital letters
 
         //this will not compile
         PI = 4;
